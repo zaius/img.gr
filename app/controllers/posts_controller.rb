eb8619c9d1d10126b76cceefe161797eff43ae6d
Daniel Newman <danielnewman@daniel-newmans-macbook-pro.local>
2009-04-04 14:16:24 -0700

    ...

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,9 @@ class PostsController < ApplicationController
     redirect_to :action => :index
   end
   
-
+  def show
+    @post = Post.find(params[:id])
+  end
   
   private
   def assign_user
