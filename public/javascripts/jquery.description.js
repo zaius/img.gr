d08027761a8e8e9a4e6d2928c3d56e3e60ad2120
James <james@jamess-macbook-air.local>
2009-04-05 15:54:02 -0700

    140 char limit (with reminder) for creating or updating image. some minor css changes

--- public/javascripts/jquery.description.js
+++ public/javascripts/jquery.description.js
@@ -1,13 +1,35 @@
-$(function(){ $('div.description').find('.edit').click(function(){
+$(function(){ 
+
+
+$('textarea#image_description').keyup(function(){
+these_chars_left = $(this).parent().find('div.chars_left');
+limitChars($(this), 140, these_chars_left);
+});
+
+
+
+$('div.description').find('.edit').click(function(){
 
 var this_description = $(this).parents('div.description');
 var this_id = this_description.parents('div.image').attr('id');
 this_text = this_description.find('span.body').text();
 
 this_description.find('span').hide();
-var edit_html = '<div class="editing"><textarea>' + $.trim(this_text) + '</textarea><button>OK</button></div>';
+var edit_html = '<div class="editing"><textarea maxlength=120>' 
++ $.trim(this_text) + 
+'</textarea><button>OK</button><div class="chars_left"></div>';
 
 this_description.append(edit_html);
+
+// textarea character count for aboutme section
+var this_textarea = this_description.find('textarea');
+var these_chars_left = this_description.find('.chars_left');
+this_textarea.keyup(function(){
+limitChars(this_textarea, 140, these_chars_left);
+});
+limitChars(this_textarea, 140, these_chars_left);
+
+
 this_description.find('button').click(function(){ 
 	
 	var new_description = this_description.find('textarea').val();
@@ -27,9 +49,29 @@ this_description.find('button').click(function(){
 });	
 
 	
-	});
+	}); // end submit description
 	
+}); // end edit description
+
+
+    
 });
 
 
-});
+function limitChars(textarea, limit, infodiv)
+{
+
+	var text = textarea.val();	
+	var textlength = text.length;
+	if(textlength > limit)
+	{
+		//$('#' + infodiv).html("You've reached the "+limit+" limit ");
+		textarea.val(text.substr(0,limit));
+		return false;
+	}
+	else
+	{
+		infodiv.html((limit - textlength) +' characters left');
+		return true;
+	}
+}
