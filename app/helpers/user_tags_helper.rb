9f57f77b711fb14559bfe3b880d1a76b8bd01e1b
Daniel Newman <danielnewman@daniel-newmans-macbook-pro.local>
2009-04-04 20:28:54 -0700

    Added bg image

--- app/helpers/user_tags_helper.rb
+++ app/helpers/user_tags_helper.rb
@@ -1,6 +1,6 @@
 module UserTagsHelper
   
   def twitter_profile_link(twitter_login)
-    '<a href="http://twitter.com/' + twitter_login + '">' + twitter_login + "</a>"
+    '<a href="http://twitter.com/' + twitter_login + '">@' + twitter_login + "</a>"
   end
 end
