bd9a5a62144ad88d2f2174b26ee5ca445110ff6d
Daniel Newman <danielnewman@daniel-newmans-macbook-pro.local>
2009-04-05 18:28:29 -0700

    prep ulllll

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find_by_login(params[:id])
-    
+    @user_tags = UserTag.find(:all, :conditions => ["twitter_login = ?",current_user.login])
     respond_to do |wants|
       wants.html {  }
     end
